519242155640b4ebb4c82460ab549c03faf5a490
joshdrink <git@joshbeveridge.ca>
2019-10-10 14:38:31 -0400

    Allows playground to accept a urlPrefix variable in order to properly link assets and subdirectories if hosted on a subdirectory or Github Pages.

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,21 @@
 
 "use strict";
 
+// URL Subdirectory ============================================================
+// This variable is designed to allow for proper relative links when publishing to a subdirectory or Github Pages. If you plan on publishing to Github Pages, set this variable to the name of your repository.
+
+let urlPrefix = "";
+
+// If this variable is used, it's important to leverage the $ROOT variable in your Twig files to ensure your assets and application links work properly when deployed.
+
+// $ROOT = /subdirectory..
+// e.g. <link async href="$ROOT/css/app.css" rel="stylesheet"> turns into <link async href="/subdirectory/css/app.css" rel="stylesheet">
+// href="$ROOT/img/image.jpg" turns into href="/subdirectory/img/image.jpg"
+
 // Requirements ================================================================
 const gulp = require('gulp');
 const { series, parallel, src, dest, watch } = require('gulp');
+const replace = require('gulp-replace');
 const sass = require('gulp-sass');
 const browsersync = require('browser-sync').create();
 const uglify = require('gulp-uglify');
@@ -29,11 +41,11 @@ const twig = require('gulp-twig');
     function browserSync(done) {
         browsersync.init({
             server: {
-                baseDir: 'cache'
+                baseDir: "cache"
             },
         });
         done();
-    }
+    };
 
     // BrowserSync Reload
     function browserSyncReload(done) {
@@ -45,9 +57,26 @@ const twig = require('gulp-twig');
 
     // Twig
     function template() {
-        return src('app/twig/*.twig')
+        return src('app/twig/**/index.twig')
         .pipe(twig())
         .pipe(dest('cache'));
+    }
+    function replacePath() {
+        return src('cache/**/*.html')
+        .pipe(replace('$ROOT', ""))
+        .pipe(dest('cache'));
+    }
+    let rootPath = "/" + urlPrefix;
+    function replaceDocsPath() {
+        if (urlPrefix == "") {
+            return src('cache/**/*.html')
+            .pipe(replace('$ROOT', ""))
+            .pipe(dest('cache'));
+        } else {
+            return src('cache/**/*.html')
+            .pipe(replace('$ROOT', rootPath))
+            .pipe(dest('cache'));
+        }
     }
 
     // Clone JS
@@ -58,7 +87,7 @@ const twig = require('gulp-twig');
 
     // JavaScript
     function js() {
-        return src('app/js/*.js')
+        return src('app/js/**/*.js')
         .pipe(concat('app.js'))
         .pipe(dest('cache/js'));
     }
@@ -76,6 +105,10 @@ const twig = require('gulp-twig');
         return src('app/img/**/*')
         .pipe(dest('cache/img'));
     }
+    function favicons() {
+        return src('app/favicons/*')
+        .pipe(dest('cache'));
+    }
 
     function moveImages() {
         return src('cache/img/**/*')
@@ -88,18 +121,14 @@ const twig = require('gulp-twig');
         .pipe(dest('docs'));
     }
     function docsCacheJS() {
-        return src('cache/js/*.js')
+        return src('cache/js/**/*.js')
         .pipe(uglify())
         .pipe(dest('docs/js'));
     }
     function docsCacheCSS() {
-        return src('cache/css/*.css')
+        return src('cache/css/**/*.css')
         .pipe(postcss([cssnano()]))
         .pipe(dest('docs/css'));
-    }
-    function docsCloneJS() {
-        return src('cache/js/clone/*.js')
-        .pipe(dest('docs/js/clone'));
     }
     function docsFavicons() {
         return src('app/favicons/*')
@@ -117,19 +146,21 @@ const twig = require('gulp-twig');
     }
 
     // Compile
-    const compile = series(cleanCache, template, moveCloneJS, js, cacheImages, compileCSS);
+    const compile = series(cleanCache, template, replacePath, moveCloneJS, js, cacheImages, compileCSS, favicons);
 
-    // docs
-    const docs = series(docsCacheHTML, docsCacheJS, docsCacheCSS, docsCloneJS, moveImages, docsFavicons);
+    // Docs
+    const docsCompile = series(cleanCache, template, replaceDocsPath, moveCloneJS, js, cacheImages, compileCSS);
+    const docs = series(docsCacheHTML, docsCacheJS, docsCacheCSS, moveImages, docsFavicons);
 
     // Watch
     function watchFiles() {
         watch('app/scss/**/*.scss', series(compile, browserSyncReload));
         watch('app/twig/**/*.twig', series(compile, browserSyncReload));
-        watch('app/js/*.js', series(compile, browserSyncReload));
+        watch('app/js/**/*.js', series(compile, browserSyncReload));
+        watch('app/img/**/*.js', series(compile, browserSyncReload));
     }
 
     // Export
-    exports.build = series(cleanDocs, compile, docs);
+    exports.build = series(cleanDocs, docsCompile, docs);
     exports.watch = series(compile, parallel(browserSync, watchFiles));
     exports.default = series(compile, parallel(browserSync, watchFiles));
